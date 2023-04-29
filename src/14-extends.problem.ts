d882836601bcccb881b81d588fc755406f7ada44
josephelias94 <jose.elias.0703@gmail.com>
2023-04-29 18:52:25 -0300

    resolve exercise 14

--- src/14-extends.problem.ts
+++ src/14-extends.problem.ts
@@ -6,20 +6,21 @@ import { Equal, Expect } from './helpers/type-utils';
  * make it more DRY?
  */
 
-interface User {
+interface IdType {
   id: string;
+}
+
+interface User extends IdType {
   firstName: string;
   lastName: string;
 }
 
-interface Post {
-  id: string;
+interface Post extends IdType {
   title: string;
   body: string;
 }
 
-interface Comment {
-  id: string;
+interface Comment extends IdType {
   comment: string;
 }
 
